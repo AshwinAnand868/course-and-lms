1a27c0b9abf8bd6f0c3c910b93fee2895705828a
AshwinAnand868 <anandashwin868@gmail.com>
2024-12-22 20:50:47 -0500

    Add mux for video storage and processing and video form components

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+
+
+interface ChapterActionsProps {
+    disabled: boolean;
+    courseId: string;
+    chapterId: string;
+    isPublished: boolean;
+}
+
+const ChapterActions = ({
+    disabled,
+    courseId,
+    chapterId,
+    isPublished
+}: ChapterActionsProps) => {
+  return (
+    <div className="flex items-center gap-x-2">
+        <Button
+            variant={"outline"}
+            disabled={disabled}
+            onClick={() => {}}
+            size={"sm"}
+        >
+            {isPublished ? "Unpublish" : "Publish"}
+        </Button>
+        <Button size={"sm"}>
+            <Trash className="h-4 w-4 " />
+        </Button>
+    </div>
+  )
+}
+
+export default ChapterActions
--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,7 +1,12 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
+import Mux from "@mux/mux-node";
 import { NextResponse } from "next/server";
 
+const {video} = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID,
+  tokenSecret: process.env.MUX_TOKEN_SECRET
+});
 export async function PATCH(
   req: Request,
   {
@@ -53,7 +58,6 @@ export async function PATCH(
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const {isPublished, ...data} = await req.json(); // separating isPublished because there's gonna be a separate api route to publish a chapter after checking all required things
-    
 
     const updatedChapter = await db.chapter.update({
       data: {
@@ -66,6 +70,36 @@ export async function PATCH(
     });
 
     // TODO: Handle video upload
+    if(data.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId
+        }
+      });
+      
+      if(existingMuxData) {
+        await video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({
+          where : {
+            id: existingMuxData.id
+          }
+        });
+      }
+
+      const asset = await video.assets.create({
+        input: data.videoUrl,
+        playback_policy: ["public"],
+        test: false
+      });
+
+      await db.muxData.create({
+        data: {
+          assetId: asset.id,
+          chapterId: params.chapterId,
+          playbackId: asset.playback_ids?.[0].id,
+        }
+      })
+    }
 
     return NextResponse.json(updatedChapter);
   } catch (error) {
