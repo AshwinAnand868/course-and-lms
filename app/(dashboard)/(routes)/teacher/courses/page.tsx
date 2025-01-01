f3c11b15c2546c740ed7ef046ee09fa4f9e2b630
AshwinAnand868 <anandashwin868@gmail.com>
2025-01-01 10:32:44 -0500

    Add shadcn data table to display courses

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,43 @@
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import { columns } from "./_components/columns"
+import { DataTable } from "./_components/data-table"
 
-const CoursesPage = () => {
+// async function getData(): Promise<Payment[]> {
+//   // Fetch data from your API here.
+//   return [
+//     {
+//       id: "728ed52f",
+//       amount: 100,
+//       status: "pending",
+//       email: "m@example.com",
+//     },
+//     // ...
+//   ]
+// }
+
+
+const CoursesPage = async () => {
+
+  const { userId } = await auth();
+
+  if(!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId: userId
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  });
+
   return (
     <div className="p-6">
-        <Link href={"/teacher/create"}>
-            <Button>New course</Button>
-        </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   )
 }
